f3d52174f24003b34f6ce7dfd2fc238851fb4bc8
yourcarrot0214 <55132793+yourcarrot0214@users.noreply.github.com>
2020-12-29 22:08:22 +0900

    ReduxExample linked css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
+// fetch api
+// import App from "./App";
 
-import App from "./App";
+// Redux
 import ReduxExample from "./components/ReduxExample";
 
 const rootElement = document.getElementById("root");
